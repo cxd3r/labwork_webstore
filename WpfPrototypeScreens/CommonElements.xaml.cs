fe606ba338b623bf49b097179bd220307b9e77db
cxd3r <yusif.bagirov.aliev@gmail.com>
2019-09-24 15:40:05 +0300

    first commit.

---
+++ WpfPrototypeScreens/CommonElements.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPrototypeScreens
+{
+	/// <summary>
+	/// Interaction logic for CommonElements.xaml
+	/// </summary>
+	public partial class CommonElements : UserControl
+	{
+		public CommonElements()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
